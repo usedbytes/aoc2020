cd2cd66d17162962d97cae1935b57aa4280670df
Brian Starkey <stark3y@gmail.com>
2020-12-13 11:49:16 +0000

    07: Bag rule parsing

---
+++ 07/puzzle.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var colorStr string = "([a-z]+ [a-z]+)"
+
+var coloredBagsStr string = colorStr + " bags"
+var coloredBagsRE *regexp.Regexp = regexp.MustCompile(coloredBagsStr)
+
+var numberedBagStr string = "([0-9]+) " + colorStr + " bag(?:s)?"
+var numberedBagRE *regexp.Regexp = regexp.MustCompile(numberedBagStr)
+
+var noOtherBagStr string = "(no other bags)"
+var noOtherBagRE *regexp.Regexp = regexp.MustCompile(noOtherBagStr)
+
+var numberedOrNoneStr string = "((?:" + numberedBagStr + ")|(?:" + noOtherBagStr + "))"
+var numberedOrNoneRE *regexp.Regexp = regexp.MustCompile(numberedOrNoneStr)
+
+type NumberedBag struct {
+	Color string
+	Count int
+}
+
+type Bag struct {
+	Color    string
+	Contents map[string]int
+}
+
+func parseBagColor(color string) (string, error) {
+	matches := coloredBagsRE.FindStringSubmatch(color)
+
+	if len(matches) != 2 {
+		return "", fmt.Errorf("couldn't parse as colored bag: %s", color)
+	}
+
+	return matches[1], nil
+}
+
+func parseBagContents(contents string) ([]NumberedBag, error) {
+	allMatches := numberedOrNoneRE.FindAllStringSubmatch(contents, -1)
+
+	if len(allMatches) == 0 {
+		return nil, fmt.Errorf("couldn't parse as bag contents: %s", contents)
+	}
+
+	numberedBags := make([]NumberedBag, 0)
+
+	for i, match := range allMatches {
+		if match[0] == "no other bags" {
+			if i != 0 || i != len(allMatches)-1 || match[4] != "no other bags" {
+				return nil, fmt.Errorf("\"no other bags\" must be the only contents: %s", contents)
+			}
+
+			return nil, nil
+		}
+
+		num, err := strconv.Atoi(match[2])
+		if err != nil {
+			return nil, err
+		}
+
+		color := match[3]
+
+		numberedBags = append(numberedBags, NumberedBag{Color: color, Count: num})
+	}
+
+	return numberedBags, nil
+}
+
+func NewBag(rule string) (*Bag, error) {
+	bag := Bag{
+		Contents: make(map[string]int),
+	}
+
+	tokens := strings.Split(rule, " contain ")
+
+	color, err := parseBagColor(tokens[0])
+	if err != nil {
+		return nil, err
+	}
+
+	bag.Color = color
+
+	contents, err := parseBagContents(tokens[1])
+	if err != nil {
+		return nil, err
+	}
+
+	for _, b := range contents {
+		bag.Contents[b.Color] = b.Count
+	}
+
+	return &bag, nil
+}
+
+func main() {
+
+}
---
+++ 07/puzzle_test.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestColoredBagRE(t *testing.T) {
+	rule := "shiny gold bags"
+	matches := coloredBagsRE.FindStringSubmatch(rule)
+
+	if len(matches) != 2 || matches[1] != "shiny gold" {
+		t.Errorf("%#v", matches)
+	}
+}
+
+func TestParseBagColor(t *testing.T) {
+	rule := "shiny gold bags"
+
+	color, err := parseBagColor(rule)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if color != "shiny gold" {
+		t.Errorf("%#v != shiny gold", color)
+	}
+}
+
+func TestNumberedBagRE(t *testing.T) {
+	rule := "3 shiny gold bags"
+	matches := numberedBagRE.FindStringSubmatch(rule)
+
+	if len(matches) != 3 || matches[1] != "3" || matches[2] != "shiny gold" {
+		t.Errorf("%#v", matches)
+	}
+
+	rule = "1 shiny gold bag"
+	matches = numberedBagRE.FindStringSubmatch(rule)
+
+	if len(matches) != 3 || matches[1] != "1" || matches[2] != "shiny gold" {
+		t.Errorf("%#v", matches)
+	}
+}
+
+func TestNoOtherBagRE(t *testing.T) {
+	rule := "no other bags"
+	matches := noOtherBagRE.FindStringSubmatch(rule)
+
+	if len(matches) != 2 || matches[1] != "no other bags" {
+		t.Errorf("%#v", matches)
+	}
+}
+
+func TestNumberedOrNoneRE(t *testing.T) {
+	rule := "1 shiny gold bag"
+	allMatches := numberedOrNoneRE.FindAllStringSubmatch(rule, -1)
+	if len(allMatches) != 1 || allMatches[0][3] != "shiny gold" {
+		t.Errorf("%#v", allMatches)
+	}
+
+	rule = "no other bags"
+	allMatches = numberedOrNoneRE.FindAllStringSubmatch(rule, -1)
+	if len(allMatches) != 1 || allMatches[0][4] != "no other bags" {
+		t.Errorf("%#v", allMatches)
+	}
+
+	bag1 := "1 shiny gold bag"
+	bag2 := "2 posh purple bags"
+	rule = bag1 + ", " + bag2
+	allMatches = numberedOrNoneRE.FindAllStringSubmatch(rule, -1)
+	if len(allMatches) != 2 || allMatches[0][3] != "shiny gold" || allMatches[1][3] != "posh purple" {
+		t.Errorf("%#v", allMatches)
+	}
+
+	bag1 = "1 shiny gold bag"
+	bag2 = "2 posh purple bags"
+	bag3 := "4 brilliant white bags"
+	rule = bag1 + ", " + bag2 + ", " + bag3 + "."
+	allMatches = numberedOrNoneRE.FindAllStringSubmatch(rule, -1)
+	if len(allMatches) != 3 || allMatches[0][3] != "shiny gold" || allMatches[1][3] != "posh purple" || allMatches[2][3] != "brilliant white" {
+		t.Errorf("%#v", allMatches)
+	}
+}
+
+func TestParseBagContents(t *testing.T) {
+
+	rule := "1 shiny gold bag"
+	contents, err := parseBagContents(rule)
+	if err != nil {
+		t.Error(err)
+	} else if len(contents) != 1 {
+		t.Errorf("%s: %#v", rule, contents)
+	} else if contents[0].Color != "shiny gold" || contents[0].Count != 1 {
+		t.Errorf("%s: %#v", rule, contents)
+	}
+
+	rule = "1 shiny gold bag, 2 posh purple bags"
+	contents, err = parseBagContents(rule)
+	if err != nil {
+		t.Error(err)
+	} else if len(contents) != 2 {
+		t.Errorf("%s: %#v", rule, contents)
+	} else if contents[0].Color != "shiny gold" || contents[0].Count != 1 {
+		t.Errorf("%s: %#v", rule, contents)
+	} else if contents[1].Color != "posh purple" || contents[1].Count != 2 {
+		t.Errorf("%s: %#v", rule, contents)
+	}
+
+	rule = "1 shiny gold bag, no other bags"
+	_, err = parseBagContents(rule)
+	if err == nil {
+		t.Errorf("%v should have failed", rule)
+	}
+
+	rule = "no other bags, 1 shiny gold bag"
+	_, err = parseBagContents(rule)
+	if err == nil {
+		t.Errorf("%v should have failed", rule)
+	}
+
+	rule = "no other bags"
+	contents, err = parseBagContents(rule)
+	if err != nil {
+		t.Error(err)
+	} else if len(contents) != 0 {
+		t.Errorf("%s: %#v", rule, contents)
+	}
+}
+
+func TestParseBag(t *testing.T) {
+	rule := "light red bags contain 1 bright white bag, 2 muted yellow bags."
+	bag, err := NewBag(rule)
+	if err != nil {
+		t.Error(err)
+	} else if bag.Color != "light red" {
+		t.Errorf("%v: %#v", rule, *bag)
+	} else if bag.Contents["bright white"] != 1 {
+		t.Errorf("%v: %#v", rule, *bag)
+	} else if bag.Contents["muted yellow"] != 2 {
+		t.Errorf("%v: %#v", rule, *bag)
+	}
+
+	rule = "faded blue bags contain no other bags."
+	bag, err = NewBag(rule)
+	if err != nil {
+		t.Error(err)
+	} else if bag.Color != "faded blue" {
+		t.Errorf("%v: %#v", rule, *bag)
+	} else if len(bag.Contents) != 0 {
+		t.Errorf("%v: %#v", rule, *bag)
+	}
+}
