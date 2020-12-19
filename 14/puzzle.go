0f802c7e5f676385fbac4e69343b59114e3c24cf
Brian Starkey <stark3y@gmail.com>
2020-12-19 19:48:44 +0000

    14: Part 2

--- 14/puzzle.go
+++ 14/puzzle.go
@@ -33,29 +33,42 @@ type Machine struct {
 	MaskSet   uint64
 	MaskClear uint64
 	Mem       map[uint64]uint64
+	MaskX     []int
+}
+
+func (m *Machine) Write(addr, value uint64) {
+	m.Mem[addr] = value
 }
 
 func run() error {
 	// Obviously storing the whole address space would be impractical,
 	// but the file is only a few hundred lines long, so worst case
 	// the map will have a few hundred entries.
+	//
+	// Edit: lol @ Part2. Now we're writing a whole bunch more locations.
+	// It still looks practical to track it as a map... but perhaps there's
+	// a better way
 	m := &Machine{
-		Mem: make(map[uint64]uint64),
+		Mem:   make(map[uint64]uint64),
+		MaskX: make([]int, 0, 36),
 	}
 
+	part2 := len(os.Args) > 2
 	if err := doLines(os.Args[1], func(line string) error {
 		if strings.HasPrefix(line, "mask = ") {
 			mask := line[len("mask = "):]
 			m.MaskSet = 0
 			m.MaskClear = 0
+			m.MaskX = m.MaskX[:0]
 			bit := uint64(1 << 35)
-			for _, b := range mask {
+			for i, b := range mask {
 				switch b {
 				case '1':
 					m.MaskSet |= bit
 				case '0':
 					m.MaskClear |= bit
 				case 'X':
+					m.MaskX = append(m.MaskX, 35-i)
 					break
 				default:
 					panic(fmt.Sprintln("invalid mask bit", b))
@@ -71,7 +84,26 @@ func run() error {
 				return err
 			}
 
-			m.Mem[addr] = (value & ^m.MaskClear) | m.MaskSet
+			if !part2 {
+				m.Mem[addr] = (value & ^m.MaskClear) | m.MaskSet
+			} else {
+				// Mask = 0 means unchanged in part 2, only set bits.
+				addr = addr | m.MaskSet
+				for cnt := 0; cnt < (1 << len(m.MaskX)); cnt++ {
+					maskAddr := addr
+					for bit := 0; bit < len(m.MaskX); bit++ {
+						// MaskX is actually in MSB-to-LSB order, but
+						// it shouldn't matter because we're doing all
+						// combinations
+						if cnt&(1<<bit) != 0 {
+							maskAddr |= (1 << m.MaskX[bit])
+						} else {
+							maskAddr &= ^(1 << m.MaskX[bit])
+						}
+					}
+					m.Write(maskAddr, value)
+				}
+			}
 		}
 		return nil
 	}); err != nil {
