f9db4b4434611611f01f25d89624be6177fa7d90
Brian Starkey <stark3y@gmail.com>
2020-12-20 23:30:49 +0000

    17: Refactor into a generic N-dimensional grid interface

--- 17/puzzle.go
+++ 17/puzzle.go
@@ -28,238 +28,10 @@ func doLines(filename string, do func(line string) error) error {
 	return nil
 }
 
-type Grid struct {
-	MinX, MinY int
-	Cells      [][]byte
-	Next       [][]byte
-}
-
-func (g *Grid) CountAround(x, y int, val byte, ignoreCentre bool) int {
-	dxs := []int{-1, 0, 1}
-	dys := []int{-1, 0, 1}
-	count := 0
-	y -= g.MinY
-	x -= g.MinX
-	for _, dy := range dys {
-		if y+dy < 0 || y+dy >= len(g.Cells) {
-			continue
-		}
-
-		row := g.Cells[y+dy]
-		for _, dx := range dxs {
-			if x+dx < 0 || x+dx >= len(row) {
-				continue
-			}
-			if ignoreCentre && dy == 0 && dx == 0 {
-				continue
-			}
-
-			if row[x+dx] == val {
-				count++
-			}
-		}
-	}
-
-	return count
-}
-
-func (g *Grid) Print() {
-	fmt.Println("X:", g.MinX, "Y:", g.MinY)
-	for _, row := range g.Cells {
-		fmt.Println(string(row))
-	}
-}
-
-func (g *Grid) Flip() {
-	for i, row := range g.Next {
-		copy(g.Cells[i], row)
-	}
-}
-
-func (g *Grid) Count(val byte) int {
-	count := 0
-	for _, row := range g.Cells {
-		for _, cell := range row {
-			if cell == val {
-				count++
-			}
-		}
-	}
-
-	return count
-}
-
 func set(a []byte, b byte) {
 	for i := range a {
 		a[i] = b
 	}
-}
-
-func (g *Grid) Expand(newMin, newMax int) {
-	newSize := (newMax - newMin) + 1
-	// X and Y size/offset always the same
-	offset := g.MinY - newMin
-	newCells := make([][]byte, newSize)
-	newNext := make([][]byte, newSize)
-
-	//fmt.Println("Grid Expand ->", newMin, newMax)
-	//fmt.Println("Offset", offset)
-	//fmt.Println("NewSize", newSize)
-
-	// Fill in the new bits below the current contents
-	if offset > 0 {
-		for i := 0; i < offset; i++ {
-			newCells[i] = make([]byte, newSize)
-			set(newCells[i], '.')
-
-			newNext[i] = make([]byte, newSize)
-			set(newNext[i], '.')
-		}
-	}
-
-	// Copy over the current contents
-	for i := 0; i < len(g.Cells); i++ {
-		newCells[i+offset] = make([]byte, newSize)
-		set(newCells[i+offset], '.')
-		copy(newCells[i+offset][offset:], g.Cells[i])
-
-		newNext[i+offset] = make([]byte, newSize)
-		set(newNext[i+offset], '.')
-		copy(newNext[i+offset][offset:], g.Next[i])
-
-	}
-
-	// Fill in the new bits after the current contents
-	if newMax >= g.MinY+len(g.Cells) {
-		for i := offset + len(g.Cells); i < newSize; i++ {
-			newCells[i] = make([]byte, newSize)
-			set(newCells[i], '.')
-
-			newNext[i] = make([]byte, newSize)
-			set(newNext[i], '.')
-		}
-	}
-
-	g.MinX = newMin
-	g.MinY = newMin
-	g.Cells = newCells
-	g.Next = newNext
-	//fmt.Println("Grid Expand. Now min:", g.MinX, g.MinY, "Size:", len(g.Cells))
-}
-
-func (g *Grid) Set(x, y int, v byte) {
-	//fmt.Println("Grid Set(", x, y, string(v), ")")
-	localX := x - g.MinX
-	localY := y - g.MinY
-	// These conditions depend on X and Y size/offset always being equal
-	if localY < 0 {
-		//fmt.Println("localY small", localY)
-		g.Expand(y, g.MinY+len(g.Next))
-	} else if localY >= len(g.Next) {
-		//fmt.Println("localY big", localY)
-		g.Expand(g.MinY, y)
-	} else if localX < 0 {
-		//fmt.Println("localX small", localX)
-		g.Expand(x, g.MinX+len(g.Next[localY]))
-	} else if localX >= len(g.Next[localY]) {
-		//fmt.Println("localX big", localX)
-		g.Expand(g.MinX, x)
-	} else {
-		g.Next[localY][localX] = v
-		// Done, don't try again
-		return
-	}
-
-	// Keep trying until it's big enough
-	g.Set(x, y, v)
-}
-
-func (g *Grid) Get(x, y int, def byte) byte {
-	localX := x - g.MinX
-	localY := y - g.MinY
-	if localY < 0 || localY >= len(g.Next) || localX < 0 || localX >= len(g.Next[localY]) {
-		return def
-	} else {
-		return g.Cells[localY][localX]
-	}
-
-	return def
-}
-
-type Grid3D struct {
-	MinZ   int
-	MinX, MaxX, MinY, MaxY int
-	Planes []*Grid
-}
-
-func (g *Grid3D) CountAround(x, y, z int, val byte, ignoreCentre bool) int {
-	dzs := []int{-1, 0, 1}
-
-	localZ := z - g.MinZ
-	count := 0
-	for _, dz := range dzs {
-		if localZ+dz < 0 || localZ+dz >= len(g.Planes) {
-			continue
-		}
-		ignoreCentre = (dz == 0)
-		count += g.Planes[localZ+dz].CountAround(x, y, val, ignoreCentre)
-	}
-
-	return count
-}
-
-func (g *Grid3D) Count(val byte) int {
-	count := 0
-	for _, p := range g.Planes {
-		pcount := p.Count(val)
-		//fmt.Println("plane count", i, pcount)
-		count += pcount
-	}
-	return count
-}
-
-func (g *Grid3D) Print() {
-	for i, plane := range g.Planes {
-		fmt.Println("Z:", i+g.MinZ)
-		plane.Print()
-		fmt.Println("--")
-	}
-}
-
-func (g *Grid3D) Flip() {
-	for _, plane := range g.Planes {
-		plane.Flip()
-	}
-}
-
-func (g *Grid3D) Expand(newMin, newMax int) {
-	newSize := (newMax - newMin) + 1
-	offset := g.MinZ - newMin
-	newPlanes := make([]*Grid, newSize)
-
-	// Fill in the new bits below the current contents
-	if offset > 0 {
-		for i := 0; i < offset; i++ {
-			newPlanes[i] = &Grid{}
-		}
-	}
-
-	// Copy over the current contents
-	for i := 0; i < len(g.Planes); i++ {
-		newPlanes[i+offset] = g.Planes[i]
-	}
-
-	// Fill in the new bits after the current contents
-	if newMax >= g.MinZ+len(g.Planes) {
-		for i := offset + len(g.Planes); i < newSize; i++ {
-			newPlanes[i] = &Grid{}
-		}
-	}
-
-	g.MinZ = newMin
-	g.Planes = newPlanes
-
-	//fmt.Println("Grid3D Expand. Now min:", g.MinZ, "Size:", len(g.Planes))
 }
 
 func min(a, b int) int {
@@ -276,50 +48,295 @@ func max(a, b int) int {
 	return b
 }
 
-func (g *Grid3D) Set(x, y, z int, v byte) {
-	//fmt.Println("Grid3D.Set(", x, y, z, string(v), ")")
-	localZ := z - g.MinZ
-
-	g.MaxX = max(x, g.MaxX)
-	g.MinX = min(x, g.MinX)
-	g.MaxY = max(y, g.MaxY)
-	g.MinY = min(y, g.MinY)
-
-	if localZ < 0 {
-		g.Expand(z, g.MinZ+len(g.Planes))
-	} else if localZ >= len(g.Planes) {
-		g.Expand(g.MinZ, z)
-	} else {
-		g.Planes[localZ].Set(x, y, v)
-		// Done, don't try again
-		return
-	}
-
-	// Try again with the new size
-	g.Set(x, y, z, v)
+type Grid1D struct {
+	// Range is [Min, Max)
+	Min, Max     int
+	Values       []byte
+	defaultValue byte
 }
 
-func (g *Grid3D) Get(x, y, z int, def byte) byte {
-	localZ := z - g.MinZ
-	if localZ < 0 {
-		return def
-	} else if localZ >= len(g.Planes) {
-		return def
+type GridND struct {
+	// Range is [Min, Max)
+	Dims         int
+	Min, Max     int
+	Values       []Gridder
+	defaultValue byte
+}
+
+func NewGrid(dimensions, min, max int, defaultValue byte) Gridder {
+	var grid Gridder
+	if dimensions == 1 {
+		grid = NewGrid1D(min, max, defaultValue)
 	} else {
-		return g.Planes[localZ].Get(x, y, def)
+		gnd := &GridND{
+			Dims:         dimensions,
+			Min:          min,
+			Max:          max,
+			Values:       make([]Gridder, max-min+1),
+			defaultValue: defaultValue,
+		}
+		for i := range gnd.Values {
+			gnd.Values[i] = NewGrid(dimensions-1, min, max, defaultValue)
+		}
+		grid = gnd
 	}
 
-	return def
+	return grid
+}
+
+func NewGrid1D(min, max int, defaultValue byte) Gridder {
+	g := &Grid1D{
+		Min:          min,
+		Max:          max,
+		Values:       make([]byte, max-min+1),
+		defaultValue: defaultValue,
+	}
+
+	set(g.Values, g.defaultValue)
+
+	return g
+}
+
+func (g *Grid1D) Dup() Gridder {
+	dup := &Grid1D{
+		Min:          g.Min,
+		Max:          g.Max,
+		Values:       make([]byte, len(g.Values)),
+		defaultValue: g.defaultValue,
+	}
+	copy(dup.Values, g.Values)
+	return dup
+}
+
+func (g *GridND) Dup() Gridder {
+	dup := &GridND{
+		Dims:         g.Dims,
+		Min:          g.Min,
+		Max:          g.Max,
+		Values:       make([]Gridder, len(g.Values)),
+		defaultValue: g.defaultValue,
+	}
+	for i := range g.Values {
+		dup.Values[i] = g.Values[i].Dup()
+	}
+
+	return dup
+}
+
+func (g *Grid1D) Set(coords []int, val byte) {
+	if len(coords) != 1 {
+		panic("wrong number of coords for Grid1D")
+	}
+
+	x := coords[0]
+
+	//fmt.Printf("Grid%dD, Min: %d, Max: %d, Set(%v, %s)\n", 1, g.Min, g.Max, coords, string(val))
+
+	if x < g.Min || x > g.Max || len(g.Values) == 0 {
+		//fmt.Println("Realloc")
+		newMin := min(g.Min, x)
+		newMax := max(g.Max, x)
+		newSize := newMax - newMin + 1
+
+		newValues := make([]byte, newSize)
+		set(newValues, g.defaultValue)
+		offset := g.Min - newMin
+		copy(newValues[offset:], g.Values)
+
+		g.Min = newMin
+		g.Max = newMax
+		g.Values = newValues
+
+		//fmt.Printf("Realloc Grid%dD, Min: %d, Max: %d, Size: %d, Set(%v, %s)\n", 1, g.Min, g.Max, newSize, coords, string(val))
+	}
+
+	g.Values[x-g.Min] = val
+}
+
+func (g *GridND) Set(coords []int, val byte) {
+	if len(coords) != g.Dims {
+		panic(fmt.Sprintf("wrong number of coords for Grid, need %d got %d", g.Dims, len(coords)))
+	}
+
+	//fmt.Printf("Grid%dD, Min: %d, Max: %d, Set(%v, %s)\n", g.Dims, g.Min, g.Max, coords, string(val))
+	first := coords[0]
+
+	if first < g.Min || first > g.Max || len(g.Values) == 0 {
+		//fmt.Println("Realloc")
+		newMin := min(g.Min, first)
+		newMax := max(g.Max, first)
+		newSize := newMax - newMin + 1
+
+		newValues := make([]Gridder, newSize)
+		offset := g.Min - newMin
+		copy(newValues[offset:], g.Values)
+
+		for i := 0; i < offset; i++ {
+			newValues[i] = NewGrid(g.Dimensions()-1, 0, 0, g.defaultValue)
+		}
+		for i := offset + len(g.Values); i < newSize; i++ {
+			newValues[i] = NewGrid(g.Dimensions()-1, 0, 0, g.defaultValue)
+		}
+
+		g.Min = newMin
+		g.Max = newMax
+		g.Values = newValues
+		//fmt.Printf("Realloc Grid%dD, Min: %d, Max: %d, Size: %d, Set(%v, %s)\n", g.Dims, g.Min, g.Max, newSize, coords, string(val))
+	}
+
+	g.Values[first-g.Min].Set(coords[1:], val)
+}
+
+func (g *Grid1D) Get(coords []int, defaultVal byte) byte {
+	if len(coords) != 1 {
+		panic("wrong number of coords for Grid1D")
+	}
+
+	x := coords[0]
+	if x < g.Min || x > g.Max || len(g.Values) == 0 {
+		return defaultVal
+	}
+	return g.Values[x-g.Min]
+}
+
+func (g *GridND) Get(coords []int, defaultVal byte) byte {
+	if len(coords) != g.Dims {
+		panic(fmt.Sprintf("wrong number of coords for Grid, need %d got %d", g.Dims, len(coords)))
+	}
+
+	//fmt.Printf("Grid%dD, Min: %d, Max: %d, Get(%v, %s)\n", g.Dims, g.Min, g.Max, coords, string(defaultVal))
+	first := coords[0]
+	if first < g.Min || first > g.Max || len(g.Values) == 0 {
+		//fmt.Println("OOB")
+		return defaultVal
+	}
+	return g.Values[first-g.Min].Get(coords[1:], defaultVal)
+}
+
+func (g *Grid1D) CountAround(coords []int, val byte, ignoreCentre bool) int {
+	ddim := []int{-1, 0, 1}
+	count := 0
+	x := coords[0] - g.Min
+	for _, dx := range ddim {
+		if x+dx < 0 || x+dx >= len(g.Values) {
+			continue
+		}
+		if ignoreCentre && dx == 0 {
+			continue
+		}
+
+		if g.Values[x+dx] == val {
+			count++
+		}
+	}
+	return count
+}
+
+func (g *GridND) CountAround(coords []int, val byte, ignoreCentre bool) int {
+	if len(coords) != g.Dims {
+		panic(fmt.Sprintf("wrong number of coords for Grid, need %d got %d", g.Dims, len(coords)))
+	}
+
+	ddim := []int{-1, 0, 1}
+	count := 0
+	first := coords[0] - g.Min
+	for _, dx := range ddim {
+		if first+dx < 0 || first+dx >= len(g.Values) {
+			continue
+		}
+		count += g.Values[first+dx].CountAround(coords[1:], val, (dx == 0) && ignoreCentre)
+	}
+	return count
+}
+
+func (g *Grid1D) Count(val byte) int {
+	count := 0
+	for _, cell := range g.Values {
+		if cell == val {
+			count++
+		}
+	}
+	return count
+}
+
+func (g *GridND) Count(val byte) int {
+	count := 0
+	for _, cell := range g.Values {
+		count += cell.Count(val)
+	}
+	return count
+}
+
+func (g *Grid1D) String() string {
+	return fmt.Sprintf("%2d: %s", g.Min, string(g.Values))
+}
+
+func (g *GridND) String() string {
+	s := fmt.Sprintf("%d dim: %d\n", g.Dims, g.Min)
+	for i, val := range g.Values {
+		s += val.String()
+		if i < len(g.Values)-1 {
+			s += "\n"
+		}
+	}
+	return s
+}
+
+func (g *Grid1D) Dimensions() int {
+	return 1
+}
+
+func (g *GridND) Dimensions() int {
+	return g.Dims
+}
+
+func (g *Grid1D) Range() [][2]int {
+	return [][2]int{
+		{g.Min, g.Max},
+	}
+}
+
+func (g *GridND) Range() [][2]int {
+	dimRanges := make([][2]int, g.Dims)
+
+	var childRanges [][][2]int
+	for _, child := range g.Values {
+		childRanges = append(childRanges, child.Range())
+	}
+
+	dimRanges[0][0] = g.Min
+	dimRanges[0][1] = g.Max
+	for dim := 0; dim < g.Dims-1; dim++ {
+		dimRanges[dim+1][0] = 0
+		dimRanges[dim+1][1] = 0
+		for _, child := range childRanges {
+			dimRanges[dim+1][0] = min(child[dim][0], dimRanges[dim+1][0])
+			dimRanges[dim+1][1] = max(child[dim][1], dimRanges[dim+1][1])
+		}
+	}
+
+	return dimRanges
+}
+
+type Gridder interface {
+	Set(coords []int, val byte)
+	Get(coords []int, defaultVal byte) byte
+	CountAround(coords []int, val byte, ignoreCentre bool) int
+	Count(val byte) int
+	Dup() Gridder
+	String() string
+	Dimensions() int
+	Range() [][2]int
 }
 
 func run() error {
 
-	grid := &Grid3D{ }
+	grid := NewGrid(3, 0, 0, '.')
 
 	y := 0
 	if err := doLines(os.Args[1], func(line string) error {
 		for x, c := range []byte(line) {
-			grid.Set(x, y, 0, c)
+			grid.Set([]int{0, y, x}, c)
 		}
 		y++
 		return nil
@@ -327,48 +344,42 @@ func run() error {
 		return err
 	}
 
-	grid.Flip()
-	grid.Print()
-
+	fmt.Println("Starting configuration:")
+	fmt.Println(grid.String())
 	for cycle := 0; cycle < 6; cycle++ {
-		minZ := grid.MinZ-1
-		maxZ := grid.MinZ+len(grid.Planes)
-		minY := grid.MinY-1
-		maxY := grid.MaxY+1
-		minX := grid.MinX-1
-		maxX := grid.MaxX+1
-		//fmt.Printf("Cycle: %d, Z: (%d)-(%d), Y: (%d)-(%d), X: (%d)-(%d)\n", cycle, minZ, maxZ, minY, maxY, minX, maxX)
-		for z := minZ; z <= maxZ; z++ {
-			for y := minY; y <= maxY; y++ {
-				for x := minX; x <= maxX; x++ {
+		ranges := grid.Range()
+		next := grid.Dup()
+		for z := ranges[0][0] - 1; z <= ranges[0][1]+1; z++ {
+			for y := ranges[1][0] - 1; y <= ranges[1][1]+1; y++ {
+				for x := ranges[2][0] - 1; x <= ranges[2][1]+1; x++ {
+					current := grid.Get([]int{z, y, x}, '.')
+					count := grid.CountAround([]int{z, y, x}, '#', true)
 					//action := "none"
-					current := grid.Get(x, y, z, '.')
-					count := grid.CountAround(x, y, z, '#', true)
 					if current == '#' {
-						if (count == 2 || count == 3) {
-							grid.Set(x, y, z, '#')
+						if count == 2 || count == 3 {
+							next.Set([]int{z, y, x}, '#')
 							//action = "keep"
 						} else {
-							grid.Set(x, y, z, '.')
+							next.Set([]int{z, y, x}, '.')
 							//action = "kill"
 						}
 					} else if current == '.' {
-						if (count == 3) {
-							grid.Set(x, y, z, '#')
+						if count == 3 {
+							next.Set([]int{z, y, x}, '#')
 							//action = "spawn"
 						}
 					}
 					//fmt.Printf("(%d, %d, %d) count: %d, action: %s\n", z, y, x, count, action)
 				}
 			}
+
 		}
-		grid.Flip()
-		//fmt.Println("After Cycle", cycle)
-		//grid.Print()
+		grid = next
+		//fmt.Println("Cycle", cycle)
+		//fmt.Println(next.String())
 		//fmt.Println("-----")
 	}
 
-	//grid.Print()
 	fmt.Println(grid.Count('#'))
 
 	return nil
