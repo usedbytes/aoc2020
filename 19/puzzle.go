b013b0eb14f151cf2b9826ed836c5e270557bac0
Brian Starkey <stark3y@gmail.com>
2020-12-23 18:42:33 +0000

    19: Part2 "the hard way"
    This looks simple now, but I fought with it for hours.
    Memoization is essential to get reasonable runtime out of this. I think
    the bug I spent all day chasing down was just the "optionIsBad" case.
    I did just 'break' in that case, but that break is nested one level too
    deep in the loops, so it wasn't doing the right thing.
    
    Anyway, I think this is probably the "hard" way alluded to on the puzzle
    page (at least, I hope so!).
    
    I couldn't really figure out the "simple" way, at least not with the way
    I did Part 1. I imagine, the "intended" solution to Part 1 is to work
    through all the rules and replace them with the concrete substrings they
    represent, then match those against the message. Presumably, for Part 2
    you'd then add a couple of exceptions, working from the front and the
    back, then attempting to replace the middle with one of the
    recursive/repeat rules.

--- 19/puzzle.go
+++ 19/puzzle.go
@@ -31,58 +31,138 @@ func doLines(filename string, do func(line string) error) error {
 }
 
 type Rule interface {
-	Validate([]byte, map[int]Rule) (bool, int)
+	Validate([]byte, map[int]Rule) (bool, []int)
 }
 
 type DerivedRule struct {
 	SubRules [][]int
+
+	Memo map[string][]int
 }
 
-func (dr *DerivedRule) Validate(data []byte, rules map[int]Rule) (bool, int) {
-	var (
-		ok bool
-		consumed int
-	)
+func in(haystack []int, needle int) bool {
+	for _, n := range haystack {
+		if n == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func (dr *DerivedRule) Validate(data []byte, rules map[int]Rule) (bool, []int) {
+
+	// Check if we've been here before
+	if res, memod := dr.Memo[string(data)]; memod {
+		return len(res) > 0, res
+	}
+
+	var endPositions []int
 	for _, option := range dr.SubRules {
-		ok = true
-		consumed = 0
-		for _, element := range option {
-			var n int
-			ok, n = rules[element].Validate(data[consumed:], rules)
-			if !ok {
+		var (
+			ruleIdx int
+			rule    int
+
+			// Each option starts at the beginning
+			startPositions []int = []int{0}
+
+			optionIsBad bool = false
+		)
+
+		for ruleIdx, rule = range option {
+			var (
+				thisEndPositions []int
+			)
+
+			for _, start := range startPositions {
+				ok, tmp := rules[rule].Validate(data[start:], rules)
+				if !ok {
+					// No successful matches for this starting position
+					continue
+				} else if len(tmp) == 0 {
+					panic("OK but no results")
+				}
+
+				for _, consumed := range tmp {
+					if consumed == 0 {
+						panic("zero consumed")
+					}
+
+					end := start + consumed
+
+					// This would use up the whole data, but we have more rules to match
+					// in this option, so that's not OK
+					if end >= len(data) && ruleIdx != len(option)-1 {
+						continue
+					}
+
+					// Don't add duplicates
+					if in(thisEndPositions, end) {
+						continue
+					}
+
+					thisEndPositions = append(thisEndPositions, end)
+				}
+			}
+
+			// thisEndPositions now contains all the ways to get "past" 'rule'
+			if len(thisEndPositions) == 0 {
+				// None of the possible starting points for this rule produced
+				// anything viable so this option can't go anywhere
+				optionIsBad = true
 				break
 			}
-			consumed += n
+
+			// The next rule starts at the end of this one
+			startPositions = thisEndPositions
 		}
-		if ok {
-			break
+		if !optionIsBad {
+			// We managed to traverse the whole option, so we can
+			// reach everything in startPositions
+			endPositions = append(endPositions, startPositions...)
 		}
 	}
 
-	return ok, consumed
+	if _, ok := dr.Memo[string(data)]; !ok {
+		dr.Memo[string(data)] = endPositions
+	}
+
+	return len(endPositions) > 0, endPositions
 }
 
 type BaseRule struct {
 	Char byte
 }
 
-func (br *BaseRule) Validate(data []byte, rules map[int]Rule) (bool, int) {
+func (br *BaseRule) Validate(data []byte, rules map[int]Rule) (bool, []int) {
 	if len(data) == 0 {
-		return false, 0
+		return false, nil
+	} else if data[0] != br.Char {
+		return false, nil
 	}
-	return data[0] == br.Char, 1
+
+	return true, []int{1}
 }
 
 func Validate(data []byte, against int, rules map[int]Rule) bool {
 	ok, consumed := rules[against].Validate(data, rules)
-	if ok && consumed == len(data) {
-		return true
+	if !ok {
+		return false
+	}
+
+	for _, c := range consumed {
+		if c == len(data) {
+			return true
+		}
 	}
 
 	return false
 }
 
 func run() error {
+	part2 := false
+	if len(os.Args) > 2 {
+		part2 = true
+	}
 
 	rules := make(map[int]Rule)
 
@@ -91,7 +171,29 @@ func run() error {
 
 	if err := doLines(os.Args[1], func(line string) error {
 		if len(line) == 0 {
+			// Done parsting rules
 			parsing = false
+
+			if part2 {
+				fmt.Println("Overriding rules (Part 2)")
+				// Override the two rules
+				// 8: 42 | 42 8
+				// 11: 42 31 | 42 11 31
+				rules[8] = &DerivedRule{
+					SubRules: [][]int{
+						{42},
+						{42, 8},
+					},
+					Memo: make(map[string][]int),
+				}
+				rules[11] = &DerivedRule{
+					SubRules: [][]int{
+						{42, 31},
+						{42, 11, 31},
+					},
+					Memo: make(map[string][]int),
+				}
+			}
 			return nil
 		}
 
@@ -112,6 +214,7 @@ func run() error {
 			} else {
 				dr := &DerivedRule{
 					SubRules: make([][]int, len(options)),
+					Memo:     make(map[string][]int),
 				}
 				rule = dr
 				for o, option := range options {
@@ -130,6 +233,7 @@ func run() error {
 			}
 			rules[ruleNo] = rule
 		} else {
+			// Validate message
 			ok := Validate([]byte(line), 0, rules)
 			if ok {
 				res++
